c40cc323f08342499e04c8c2ddb5c761d3655e05
Jack <toastlinaker@rocketmail.com>
2023-12-08 20:54:56 +0000

    Update faces

--- week-0/faces/faces.py
+++ week-0/faces/faces.py
@@ -1,8 +1,10 @@
+# Create main function that takes input, applies convert function to input, then prints converted input
 def main():
     text = input("Type something with a happy or sad face! ")
     textcon = convert(text)
     print(textcon)
 
+# Create function that replaces ":)" and ":(" with corresponding emoji
 def convert(n):
     return n.replace(":)", "🙂").replace(":(", "🙁")
 
